ca0c9d076d6dd20ce5b1018d19f5797af234a6b1
leonardo-arthur <gitleoarthur@gmail.com>
2025-09-08 10:33:28 -0300

    03 - Função para buscar CEP na API

---
+++ assets/js/cep.js
@@ -0,0 +1,49 @@
+// Recuperando o formulário + o campo de CEP + o botão de CEP
+const cepInput = document.querySelector("#cep");
+const buscarCepBtn = document.querySelector("#buscarCep");
+const form = document.querySelector("#enderecoForm");
+
+// Montando a função que chama a API e retorna os dados do CEP
+async function buscarCep(){
+    const cep = cepInput.value;
+
+    // verificando se o cep tem 08 dígitos
+    if(cep.length === 8){
+        try {
+            const res = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+            const data = await res.json();
+            if (!data.erro){
+                document.querySelector('#logradouro').value = data.logradouro;
+                document.querySelector('#bairro').value = data.bairro;
+                document.querySelector('#cidade').value = data.localidade;
+                document.querySelector('#estado').value = data.uf;
+
+                // colocando o cursor no campo para digitar o número
+                document.querySelector('#numero').focus();
+            } else {
+                alert('CEP não encontrado!');
+                cepInput.focus();
+            }
+        } catch (error) {
+            alert('Erro ao buscar o CEP: ', error);
+        }
+    } else {
+        alert('Digite um CEP com 08 números!');
+        cepInput.focus();
+    }
+}
+
+// Chamar a busca do cep, quando clicar no botão
+
+buscarCepBtn.addEventListener('click', buscarCep);
+
+
+// máscara automática e permissão de apenas números
+
+cepInput.addEventListener('input', (e) => {
+  let valorDigitado = e.target.value.replace(/\D/g, '');
+  if (valorDigitado.length > 5){
+    valorDigitado = valorDigitado.slice(0, 5) + '-' + valorDigitado.slice(5, 8)
+  }
+  e.target.value = valorDigitado;
+})
